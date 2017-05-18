df17d241315542037ab3e3c7a817998b9ea8ea98
yamapi06150 <yamapi06150@gmail.com>
2017-05-18 18:48:07 +0800

    CommentBox：it has a text area

--- test/components/CommentBox_test.js
+++ test/components/CommentBox_test.js
@@ -4,7 +4,10 @@ import CommentBox from '../../src/components/CommentBox.jsx' ;
 describe('CommentBox', () => {
     it('has the correct class', () => {
         const component = renderComponent(CommentBox);
-        console.log(component);
         expect(component).to.have.class('comment-box');
+    });
+    it('has a text area', () => {
+        const component = renderComponent(CommentBox);
+        expect(component.find('textarea')).to.exist;
     });
 });
