734c184da38ad591d21abdebab285561c4efcc65
yamapi06150 <yamapi06150@gmail.com>
2017-05-19 14:11:50 +0800

    complete all react side tests

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,11 +1,13 @@
 import React, {Component} from 'react' ;
+import {connect} from 'react-redux' ;
 
-class CommentList extends Component {
-    render(){
-        return (
-            <ul className="comment-list"></ul>
-        );
-    }
+const CommentList = (props) => {
+    const list = props.comments.map( (comment, index) => (
+        <li key={index}>{comment}</li>
+    ) );
+    return (<ul className="comment-list">{list}</ul>);
 }
 
-export default CommentList ;
+export default connect( ({comments}) => ({
+    comments
+}) )(CommentList) ;
